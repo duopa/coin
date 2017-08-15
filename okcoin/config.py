289983ac9ff38dbc871e85e107615d389a9137dc
lockeduan <lockeduan@outlook.com>
2017-08-15 22:43:46 +0800

    update config

--- okcoin/config.py
+++ okcoin/config.py
@@ -7,7 +7,7 @@ url_cn = "www.okcoin.cn"
 url_com = "www.okcoin.com"
 
 config_3min = {
-    "stop_profit_ratio": 0.03,
+    "stop_profit_ratio": 0.025,
     "stop_loss_ratio": 0.045,
     "short_ratio": [0.2, 0.3, 0.5],
     "coin_most_hold_ratio": 0.3,
