d9a9a981b7f49c0a0e434fe07b172003e94af0d7
lockeduan <lockeduan@outlook.com>
2017-08-20 08:47:38 +0800

    update

--- strategy/emacrosswithmacd.py
+++ strategy/emacrosswithmacd.py
@@ -78,7 +78,7 @@ class EmaCrossWithMacdStrategy(StrategyBase):
         #: if quick from top approching slow enough, then deem it's dead cross
         approch_a = (self._ema_quick[-1] - self._ema_slow[-1]) / self._ema_slow[-1]
         approch_b = (self._ema_quick[-2] - self._ema_slow[-2]) / self._ema_slow[-2]
-        if approch_a < 0.001 and approch_b < 0.001:
+        if 0 < approch_a < 0.001 and 0 < approch_b < 0.001:
             return True
 
         has_crossed = self._ema_quick[-1] < self._ema_slow[-1] \
