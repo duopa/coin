4e87fd909e35b670434134bbae8c335a88bef1a9
Locke (Hongxing) Duan <->
2017-08-21 17:44:00 +0800

    udpate config

--- okcoin/config.py
+++ okcoin/config.py
@@ -31,7 +31,7 @@ config_5min = {
 }
 
 eamcrosswithmacd_config_3min = {
-    "stop_profit_ratio": 0.005,#basically, any time dead cross then short
+    "stop_profit_ratio": 0.008,#basically, any time dead cross then short
     "stop_loss_ratio": 0.045,
     "short_ratio": [0.6, 1],
     "coin_most_hold_ratio": 0.3,
