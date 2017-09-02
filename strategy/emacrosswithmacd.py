a10958f9d0306645b9723ef1b20f3c2b76682888
lockeduan <lockeduan@outlook.com>
2017-09-02 22:08:56 +0800

    upate

--- strategy/emacrosswithmacd.py
+++ strategy/emacrosswithmacd.py
@@ -38,7 +38,8 @@ class EmaCrossWithMacdStrategy(StrategyBase):
         macd_signal = (macd_golden_cross or macd_slope_signal) and self._is_long_price_under_last_dead_cross_price_percent(long_price)
         '''
         is_on_ranging = self._is_on_ranging()
-        macd_signal = self._is_assistant_dif_slope_positive() and self._is_macd_golden_cross() #and self._is_long_price_under_last_dead_cross_price_percent(long_price)
+        #macd_signal = self._is_assistant_dif_slope_positive() and self._is_macd_golden_cross() #and self._is_long_price_under_last_dead_cross_price_percent(long_price)
+        macd_signal = self._is_macd_slope_change_to_positive()
         ema_golden_cross = self._is_ema_golden_cross() #and self._is_long_price_under_highest_price_percent(long_price)
         has_four_green = self._has_four_green()
         if (macd_signal or ema_golden_cross or has_four_green) and not is_on_ranging:
@@ -164,6 +165,24 @@ class EmaCrossWithMacdStrategy(StrategyBase):
                 return True
         return False
 
+    def _is_macd_slope_change_to_positive(self):
+        changed = self._macd[-1] > self._macd[-2]
+        if changed:
+            #make sure the last dea smaller than the min hist bar
+            min_hist = numpy.min(self._macdhist[-90:])
+            if min_hist >= 0 or math.isnan(min_hist) or self._macdsignal[-1] > (min_hist * 2):# okcoin's macd hist doubled? don't why?
+                return False
+            i = -1
+            while i >= -12:
+                if self._macd[i] < self._macdsignal[i]:
+                    i -= 1
+                    continue
+                else:
+                    return False
+            return True
+        else:
+            return False
+    #-----------------------------------------------------------------------------------------------
     def _is_assistant_dif_slope_positive(self):
         #: if no assistant, ignore
         if not self._kline_assistant:
@@ -183,7 +202,7 @@ class EmaCrossWithMacdStrategy(StrategyBase):
         opens = self._get_value_from_kline(four_kline, 1)
         has_four_green = True
         #:make sure open below close
-        for i in range(-1, -5, -1): 
+        for i in range(-1, -5, -1):
             if opens[i] < self._close[i]:
                 continue
             else:
